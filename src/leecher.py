2c1b04c3f3673fd9bfa2e3d9a94c61c62d9b1c65
Ethan Ngwetjana <ngweth001@myuct.ac.za>
2025-03-03 17:34:57 +0200

    Added file sending funcionality

--- src/leecher.py
+++ src/leecher.py
@@ -14,7 +14,7 @@ leechSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 leechSocket.connect(ADDR)
 
 # send message to seeder
-def send(msg):
+def send_text(msg):
     message = msg.encode(FORMAT)
     
     #send message
@@ -22,12 +22,28 @@ def send(msg):
     
     #print response from server (seeder)
     print(leechSocket.recv(2048).decode(FORMAT))
-    
 
+def get_file(filename):
+    file = open(filename, "wb")
     
+    try:
+        while True:
+            data = leechSocket.recv(4096) #recieve in chunks
+            if not data:
+                break
+            file.write(data)
+            
+    finally:
+        print(f"File recieved successfully and saved as '{filename}'")
+        file.close()
+        
+    leechSocket.close()
+        
+        
     
-input = input("Input something:\n")
-send(input)
+input = input("What would you like to do?\n")
+send_text(input)
+get_file("test_out.txt")
 
 
 
--- src/seeder.py
+++ src/seeder.py
@@ -1,5 +1,4 @@
 # seeder receives message from leecher
-
 import socket 
 import time
 
@@ -8,9 +7,30 @@ HEADER = 64 # the size of the message will be a minimum of 64 bytes
 PORT = 5050
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = '!exit'
+GET_FILE_MESSAGE = "file"
 SERVER = socket.gethostname()
 ADDR = (SERVER, PORT)
+filename = "test_in.txt"
 
+
+#send a file to the leecher
+def send_file(filename, conn):
+    #open file in byte mode
+    file = open(filename, "rb")
+    
+    try:
+        while True:
+            # read in chunks
+            chunk = file.read(4096)
+            if not chunk:
+                break
+            conn.send(chunk)
+    finally:
+        print("File sent successfully.")
+        file.close()
+        
+        
+    
 
 
 # create seeder socket and bind to address (TCP connection)
@@ -18,7 +38,7 @@ seedSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 seedSocket.bind(ADDR)
 
  
-#after handshake listen for messages from TCP client (leecher)   
+#listen for messages from TCP client (leecher)   
 def start():
     seedSocket.listen(1)
     print(f"[LISTENING] Server is listening on {SERVER}")
@@ -33,11 +53,16 @@ def start():
         activeCount += 1
         print(f"Connected by {addr} | Total leechers: {activeCount}")
     
-        
         message = conn.recv(1024).decode(FORMAT)
         
         print(f"Client said '{message}'")
-        conn.send("Msg recieved :)".encode(FORMAT))
+        
+        
+        if (message == GET_FILE_MESSAGE):
+            conn.send("Sending file...".encode(FORMAT))
+                
+            send_file(filename, conn)
+        
          
         conn.close()
         
