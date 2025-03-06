2eb5fb468751b9adb7a9c5adb82f117189e1b07e
Siyabonga Madondo <mdnsiy014@myuct.ac.za>
2025-03-06 22:53:30 +0000

    PyTorrent - Obtained leecher and seeder code from Ethan's branch.

--- src/leecher.py
+++ src/leecher.py
@@ -14,3 +14,80 @@ class Leecher:
     :author: Siyabonga Madondo, Ethan Ngwetjana, Lindokuhle Mdlalose
     :version: 17/03/2025
     """    
+    def __init__(self, peer_id, ip_address, port):
+        
+        """
+        :param peer_id: Identifier for the peer
+        :param ip_address: IP address for the leecher
+        :param port: The port number the leecher is listening on
+        """
+        
+        self.peer_id = peer_id
+        self.ip_address = ip_address
+        self.port = port
+        
+        self.addr = (self.ip_address, self.port)
+        
+        #Socket for TCP connection with a seeder
+        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    def register_with_tracker():
+        """
+        Establish a connection with the tracker
+        """   
+        pass
+    
+    def connect_with_seeder(self):
+        """
+        Establish a TCP connection with a seeder
+        """
+        self.tcp_socket.connect(self.addr)
+        
+    
+    def connect_with_seeders():
+        """
+        Establish a TCP connection with multiple seeders in parallel
+        """
+        pass
+        
+        
+    def download_chunk():
+        """
+        Download chunk from a seeder
+        """
+        pass
+          
+    def download_file(self):
+        """
+        Download download file from seeder (temporary)
+        """
+        #recieve filename
+        filename = self.tcp_socket.recv(4096).decode()
+        
+        file = open(filename, "wb")
+        
+        try:
+            while True:
+                data = self.tcp_socket.recv(4096) #recieve in chunks
+                if not data:
+                    break
+                #write recieved chunks in file
+                file.write(data)
+                
+        finally:
+            print(f"File recieved successfully and saved as '{filename}'")
+            file.close()
+            
+        self.tcp_socket.close()
+         
+    def resassemble_file():
+        """
+        Reassemble file from the downloaded chunks
+        """
+        pass
+        
+    def become_seeder():
+        """
+        Become a seeder after downloading a file
+        """
+        pass
--- src/seeder.py
+++ src/seeder.py
@@ -13,3 +13,86 @@ class Seeder:
     :author: Siyabonga Madondo, Ethan Ngwetjana, Lindokuhle Mdlalose
     :version: 17/03/2025
     """    
+    def __init__(self, peer_id, ip_address, port, file_path):
+        
+        
+        self.peer_id = peer_id
+        self.ip_address = ip_address
+        self.port = port
+        self.file_path = file_path
+        
+        self.addr = (self.ip_address, self.port)
+        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+               
+    def split_into_chuncks():
+        """
+        Host a file and divide it into chuncks
+        """
+        pass
+    
+    def register_with_tracker():
+        """
+        Establish a connection with the tracker
+        """
+        pass
+    
+    def connect_with_leecher(self):
+        """
+        Establish a TCP connection with a leecher
+        """
+        self.tcp_socket.bind(self.addr)
+        
+         #listen for new connections
+        self.tcp_socket.listen(1)
+        print(f"[LISTENING] Server is listening on {self.ip_address}")
+              
+        while True: #Wait for msg
+            
+            #create new socket for sending messages
+            conn, addr = self.tcp_socket.accept()
+            
+            #start a thread to send a file
+            thread = threading.Thread(target=send_file, args=(self.file_path, conn))
+            thread.start()
+
+            print(f"Connected by {addr} | Total leechers: {threading.active_count() - 1}")
+ 
+    def send_file(filename, conn):
+        """
+        Send a file to the leecher (temporary)
+        """
+        wait = True
+        while wait: #wait for signal to start sending
+            
+            #get signal to start sending
+            msg = conn.recv(4096).decode()
+            
+            if msg == DOWNLOAD_MESSAGE:
+                wait = False
+                
+        #send the filename of file being sent
+        conn.send(filename.encode())
+                 
+        #open file in byte mode
+        file = open(filename, "rb")
+              
+        try:
+            while True:
+                # read in chunks
+                chunk = file.read(4096)
+                if not chunk:
+                    break
+                #send chunks
+                conn.send(chunk)
+        finally:
+            print("File sent successfully.")
+            file.close()
+            
+            #after file is sent close connection
+            conn.close()
+        
+    def send_chuncks():
+        """
+        Serve file chuncks to the leecher
+        """
+        pass
