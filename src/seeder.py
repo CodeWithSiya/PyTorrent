8afcf0d585f0f62043bee423441649f7d4a6fd02
Siyabonga Madondo <mdnsiy014@myuct.ac.za>
2025-03-07 11:55:46 +0000

    PyTorrent - Initialised basic seeder in seeder.py

--- src/seeder.py
+++ src/seeder.py
@@ -13,86 +13,107 @@ class Seeder:
     :author: Siyabonga Madondo, Ethan Ngwetjana, Lindokuhle Mdlalose
     :version: 17/03/2025
     """    
-    def __init__(self, host:  peer_id, ip_address, port, file_path):
+    
+    #TODO: Speak to group about the home directory apporach.
+    
+    def __init__(self, host: str, udp_port: int, tcp_port: int, tracker_timeout: int = 30, file_path: str):
+        """
+        Initialises the Seeder with the given host, UDP port, TCP port, tracker timeout and file path.
         
+        :param host: The host address of the seeder.
+        :param udp_port: The UDP port on which the tracker listens for incoming connections.
+        :param tcp_port: The TCP port on which the leecher listens for incoming file requests.
+        :param tracker_timeout: Time (in seconds) to wait before considering the tracker as unreachable.
+        :param file_path: Path to the file to be shared.
+        """
+        # Configuring the leecher details.
+        self.host = host
+        self.udp_port = udp_port
+        self.tcp_port = tcp_port
+        self.tracker_timeout = tracker_timeout
+        self.file_path = file_path # TO BE CHANGED SO DON'T WORRY ABOUT IT TOO MUCH.
         
-        self.peer_id = peer_id
-        self.ip_address = ip_address
-        self.port = port
-        self.file_path = file_path
+        # Initialise the UDP socket for tracker communication.
+        self.udp_socket = socket(AF_INET, SOCK_DGRAM)
+        self.udp_socket.bind((self.host, self.udp_port))
         
-        self.addr = (self.ip_address, self.port)
-        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Initialise the TCP socket for leecher connections.
+        self.tcp_socket = socket(AF_INET, SOCK_STREAM)
+        self.tcp_socket.bind((self.host, self.tcp_port))
+        
+        # Start the TCP server in a seperate thread.
+        self.tcp_server_thread = Thread(target=self.start_tcp_server, daemon=True)
+        self.tcp_server_thread.start()
                
-    def split_into_chuncks():
-        """
-        Host a file and divide it into chuncks
-        """
-        pass
+    # def split_into_chuncks():
+    #     """
+    #     Host a file and divide it into chuncks
+    #     """
+    #     pass
     
-    def register_with_tracker():
-        """
-        Establish a connection with the tracker
-        """
-        pass
+    # def register_with_tracker():
+    #     """
+    #     Establish a connection with the tracker
+    #     """
+    #     pass
     
-    def connect_with_leecher(self):
-        """
-        Establish a TCP connection with a leecher
-        """
-        self.tcp_socket.bind(self.addr)
+    # def connect_with_leecher(self):
+    #     """
+    #     Establish a TCP connection with a leecher
+    #     """
+    #     self.tcp_socket.bind(self.addr)
         
-         #listen for new connections
-        self.tcp_socket.listen(1)
-        print(f"[LISTENING] Server is listening on {self.ip_address}")
+    #      #listen for new connections
+    #     self.tcp_socket.listen(1)
+    #     print(f"[LISTENING] Server is listening on {self.ip_address}")
               
-        while True: #Wait for msg
+    #     while True: #Wait for msg
             
-            #create new socket for sending messages
-            conn, addr = self.tcp_socket.accept()
+    #         #create new socket for sending messages
+    #         conn, addr = self.tcp_socket.accept()
             
-            #start a thread to send a file
-            thread = threading.Thread(target=send_file, args=(self.file_path, conn))
-            thread.start()
+    #         #start a thread to send a file
+    #         thread = threading.Thread(target=send_file, args=(self.file_path, conn))
+    #         thread.start()
 
-            print(f"Connected by {addr} | Total leechers: {threading.active_count() - 1}")
+    #         print(f"Connected by {addr} | Total leechers: {threading.active_count() - 1}")
  
-    def send_file(filename, conn):
-        """
-        Send a file to the leecher (temporary)
-        """
-        wait = True
-        while wait: #wait for signal to start sending
+    # def send_file(filename, conn):
+    #     """
+    #     Send a file to the leecher (temporary)
+    #     """
+    #     wait = True
+    #     while wait: #wait for signal to start sending
             
-            #get signal to start sending
-            msg = conn.recv(4096).decode()
+    #         #get signal to start sending
+    #         msg = conn.recv(4096).decode()
             
-            if msg == DOWNLOAD_MESSAGE:
-                wait = False
+    #         if msg == DOWNLOAD_MESSAGE:
+    #             wait = False
                 
-        #send the filename of file being sent
-        conn.send(filename.encode())
+    #     #send the filename of file being sent
+    #     conn.send(filename.encode())
                  
-        #open file in byte mode
-        file = open(filename, "rb")
+    #     #open file in byte mode
+    #     file = open(filename, "rb")
               
-        try:
-            while True:
-                # read in chunks
-                chunk = file.read(4096)
-                if not chunk:
-                    break
-                #send chunks
-                conn.send(chunk)
-        finally:
-            print("File sent successfully.")
-            file.close()
+    #     try:
+    #         while True:
+    #             # read in chunks
+    #             chunk = file.read(4096)
+    #             if not chunk:
+    #                 break
+    #             #send chunks
+    #             conn.send(chunk)
+    #     finally:
+    #         print("File sent successfully.")
+    #         file.close()
             
-            #after file is sent close connection
-            conn.close()
+    #         #after file is sent close connection
+    #         conn.close()
         
-    def send_chuncks():
-        """
-        Serve file chuncks to the leecher
-        """
-        pass
+    # def send_chuncks():
+    #     """
+    #     Serve file chuncks to the leecher
+    #     """
+    #     pass
